bf4b9e283d9fbe22f8da9710b0d5622a0b30064e
gi-sivajan <140698415+gi-sivajan@users.noreply.github.com>
2023-09-11 22:49:32 -0700

    9/12 Update
    Significant improvements, including the option to avoid ambiguous characters for increased memorability + an easier and clearer UX.

--- lavia.cpp
+++ lavia.cpp
@@ -2,41 +2,61 @@
 #include <cstdlib>
 #include <ctime>
 
-// Function to generate a random password
-std::string generatePassword(int length) {
-    const std::string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+std::string generatePassword(int length, bool includeAmbiguous) 
+{
+    const std::string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const std::string specialCharacters = "!@#$%^&*()_+";
+    const std::string ambiguousCharacters = "l1O0"; // Ambiguous characters
     const int charCount = characters.length();
+    const int specialCharCount = specialCharacters.length();
+    const int ambiguousCharCount = ambiguousCharacters.length();
     std::string password;
 
     for (int i = 0; i < length; i++) {
-        int randomIndex = rand() % charCount;
-        password += characters[randomIndex];
+        int randomIndex;
+
+        if (!includeAmbiguous) {
+            randomIndex = rand() % (charCount + specialCharCount);
+            if (randomIndex >= charCount) {
+                password += specialCharacters[randomIndex - charCount];
+            } else {
+                password += characters[randomIndex];
+            }
+        } else {
+            randomIndex = rand() % (charCount + specialCharCount + ambiguousCharCount);
+            if (randomIndex < charCount) {
+                password += characters[randomIndex];
+            } else if (randomIndex < charCount + specialCharCount) {
+                password += specialCharacters[randomIndex - charCount];
+            } else {
+                password += ambiguousCharacters[randomIndex - charCount - specialCharCount];
+            }
+        }
     }
 
     return password;
 }
 
 int main() {
-    int length = 12; // Default password length
+    int length;
+    bool includeAmbiguous = true;
+    
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    while (true) {
-        std::cout << "Lavia: A Super Simple Password Generator" << std::endl;
-        std::cout << "Length: " << length << std::endl;
-        std::cout << "Password: " << generatePassword(length) << std::endl;
-        std::cout << "Press 'q' to quit, '+' to increase length, or '-' to decrease length: ";
+    std::cout << "Lavia: A Super Simple Password Generator" << std::endl;
+    
+    std::cout << "Enter a password length, in numbers (19, not nineteen): ";
+    std::cin >> length;
 
-        char ch;
-        std::cin >> ch;
-
-        if (ch == 'q') {
-            break;
-        } else if (ch == '+') {
-            length++;
-        } else if (ch == '-' && length > 1) {
-            length--;
-        }
+    std::cout << "Include ambiguous characters such as 0 and O? (y/n): ";
+    char choice;
+    std::cin >> choice;
+    if (choice == 'n' || choice == 'N') {
+        includeAmbiguous = false;
     }
 
-    return 0;
-}
+    // Generate and print the password
+    std::cout << "Length: " << length << std::endl;
+    std::cout << "Password: " << generatePassword(length, includeAmbiguous) << std::endl;
+
+    return 0;} // Code to indicate a successful program termination
